88e29d4cd9deea1e6668a8b80e65bc4ab24536fb
maxdesalle <7d97b96a-bcfd-4052-95f3-cddd54e160e7@phronea.com>
2022-05-12 16:32:49 +0200

    fix(auth, users): prevent duplicate users in db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
 	getHomePage(): string {
-		return '<p>Welcome! Please <a href="/login/42">log in</a></p>';
+		return '<p>Welcome! Head over to the <a href="/login/">login page</a> to get started.</p>';
+	}
+
+	getLoginPage(): string {
+		return '<p>Click <a href="/login/42">here</a> to log in.</p>';
+	}
+
+	getErrorLoginPage(error: string): string {
+		return `<p>${error}<br><br>Click <a href="/login/42">here</a> to log in.</p>`;
 	}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,18 +7,27 @@ import { Users } from './entities/user.entity';
 export class UsersService {
 	constructor(
 		@InjectRepository(Users)
-		private usersRepository: Repository<Users>) {}
+		private usersRepository: Repository<Users>,
+	) {}
 
-	createNewUser(username: string) {
-		const new_user = new Users();
-		new_user.username = username;
-		return this.usersRepository.save(new_user);
+	async createNewUser(username: string) {
+		let user = await this.findByUsername(username);
+		if (user != undefined) {
+			return user;
+		}
+		user = new Users();
+		user.username = username;
+		await this.usersRepository.save(user);
+		return user;
 	}
 
 	findById(id: number) {
 		return this.usersRepository.findOne(id);
 	}
 
+	findByUsername(username: string): Promise<Users | undefined> {
+		return this.usersRepository.findOne({ username });
+	}
 	findAll() {
 		return this.usersRepository.find();
 	}
