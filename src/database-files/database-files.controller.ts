e99bdbb83bf7fb363ca2db596e0a91681efb5733
Rodolpho <rodolphocammrosano@gmail.com>
2022-05-24 15:42:58 +0200

    get profile image via /database-files/:id

---
+++ src/database-files/database-files.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Param, ParseIntPipe, Res, StreamableFile } from '@nestjs/common';
+import { Response } from 'express';
+import { Readable } from 'stream';
+import { DatabaseFilesService } from './database-files.service';
+
+@Controller('database-files')
+export class DatabaseFilesController {
+	constructor(
+		private readonly databaseFilesService: DatabaseFilesService
+	) {}
+
+	@Get(':id')
+	async getDatabaseFileById(
+		@Param('id', ParseIntPipe)id: number,
+		@Res({ passthrough: true }) response: Response
+	) {
+
+		const file = await this.databaseFilesService.getFileById(id);
+		const stream = Readable.from(file.data);
+
+		response.set({
+			'Content-Type': 'image'
+		})
+
+		return new StreamableFile(stream);
+	}
+}
