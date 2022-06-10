03db24c32b6f858d8d829903605283a8ca8d3fe1
Rodolpho <rodolphocammrosano@gmail.com>
2022-06-10 21:03:22 +0200

    removed no longer necessary chat-user controller and service

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from 'src/config/jwt.config';
-import { ChatUserController } from './chat-user.controller';
-import { ChatUserService } from './chat-user.service';
-import { JwtChatStrategy } from './strategy/jwt.strategy';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  controllers: [ChatUserController, ChatController],
-  providers: [ChatUserService, JwtChatStrategy, ChatService],
+  controllers: [ChatController],
+  providers: [ChatService],
   imports: [JwtModule.registerAsync(jwtConfig), UsersModule],
-  exports: [ChatUserService]
 })
 export class ChatModule {}
