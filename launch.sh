a7e3542a8c8a02f3db0edbf4dc446fdd9b7d2e4f
BeBel42 <lefevre.martin@outlook.com>
2022-07-24 16:32:18 +0200

    reindented launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check for .env file
 if [ ! -f "backend/.env" ]; then
 	echo 'Error: Missing backend/.env' 1>&2
 	exit 1
@@ -8,15 +9,17 @@ fi
 set -e
 set -x
 
+# Launch frontend
 cd frontend
 if [ ! -d "node_modules" ]; then
-  npm install
+ 	npm install
 fi
 npm run dev & P=$!
 
+# Launch backend and db
 cd ../backend
 if [ ! -d "node_modules" ]; then
-  npm install
+	npm install
 fi
 docker-compose up -d
 npm run start:dev
