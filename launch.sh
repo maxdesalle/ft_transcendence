bd6b1d3a947af6a0b320925a6bb51b8d75ea369a
BeBel42 <lefevre.martin@outlook.com>
2022-08-10 12:02:07 +0200

    made a better cleanup in launch.sk and added .pid_list in gitignore

--- launch.sh
+++ launch.sh
@@ -2,20 +2,35 @@
 
 set -e
 
+PID_FILE_NAME='.pid_list'
 ROOT_DIR=$(pwd)
-FRONTEND_PID=''
 
-kill_frontend() {
-    if ! [[ $FRONTEND_PID = '' ]]; then
-        kill $FRONTEND_PID
-    fi
-    FRONTEND_PID=''
+# execute if --stop argument provided
+kill_all() {
+    docker-compose --file $ROOT_DIR/backend/docker-compose.yml down
+    [[ -f $PID_FILE_NAME ]] || return 0
+    grep FRONTEND_PID $ROOT_DIR/$PID_FILE_NAME | awk '{print $2}' | xargs kill
+    grep BACKEND_PID $ROOT_DIR/$PID_FILE_NAME | awk '{print $2}' | xargs kill
+    rm $ROOT_DIR/$PID_FILE_NAME
 }
+
+# cleanup function
+kill_subprocesses() {
+    cat <<< "$(jobs -p)"
+    for proc in $(jobs -p);
+    do
+        kill $proc
+    done
+    # so that kill_all does not kill again the processes
+    [[ -f "$ROOT_DIR/$PID_FILE_NAME" ]] && rm "$ROOT_DIR/$PID_FILE_NAME"
+}
+# cleaning everything if ^C is pressed
+trap kill_subprocesses SIGINT
 
 check_env() {
     if [ ! -f "$ROOT_DIR/backend/.env" ]; then
         echo 'Error: Missing backend/.env' 1>&2
-        kill_frontend
+        kill_subprocesses
         exit 1
     fi
 }
@@ -39,22 +54,27 @@ exec_backend () {
     npm install
     # Execute docker db if no argument passed
     [[ $1 = '--no-db' ]] || docker-compose up -d
-    npm run start:dev
+    npm run start:dev & BACKEND_PID=$!
+    echo BACKEND_PID $BACKEND_PID >> $ROOT_DIR/$PID_FILE_NAME
+    wait $BACKEND_PID
 }
 
 exec_frontend() {
     cd $ROOT_DIR/frontend
     npm install
     npm run dev & FRONTEND_PID=$!
+    echo FRONTEND_PID $FRONTEND_PID >> $ROOT_DIR/$PID_FILE_NAME
 }
 
-exec_all () {
+exec_all() {
     exec_frontend
     exec_backend $1
 }
 
 # Argument handling
-if [[ $# = 1 && $1 = '--build' ]]; then
+if [[ $# = 1 && $1 = '--stop' ]]; then
+    kill_all
+elif [[ $# = 1 && $1 = '--build' ]]; then
     build_backend
     build_frontend
 elif [[ $# = 1 && $1 = '--no-db' ]]; then
@@ -66,12 +86,13 @@ elif [[ $# = 0 ]]; then
     build_frontend
     exec_all
 else
-    echo 'launch.sh -- build/launch ft_transcendence
+    echo "launch.sh -- build/launch ft_transcendence
     (none)              --> builds (if necessary) and launches transcendance
     --build             --> builds (if nexessary) transcendence
-    --no-db             --> builds (if nexessary) and runs transcendence without the database' 1>&2
+    --no-db             --> builds (if nexessary) and runs transcendence without the database
+    --stop              --> stops transcendence's running processes" 1>&2
     exit 1
 fi
 
-# To make sure frontend does not block the script
-kill_frontend
+# cleaning processes
+kill_subprocesses
