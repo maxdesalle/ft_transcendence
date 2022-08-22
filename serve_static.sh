a12778b35374e685f6dc6c2535f84017279b6e98
Rodolpho <rodolphocammarosano@gmail.com>
2022-08-22 18:33:21 +0200

    changed script a bit

--- serve_static.sh
+++ serve_static.sh
@@ -1,7 +1,7 @@
 export VITE_HOST=127.0.0.1
-echo "Host IP is set to" $VITE_HOST "- make sure this matches your .env file!"
-# cd frontend && npm run build
-# cd ..
+echo "!! Host IP is set to" $VITE_HOST "- make sure this matches your .env file!!"
+cd frontend && npm run build
+cd ..
 
 cd backend
 
