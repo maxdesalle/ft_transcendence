b972949523b4542bc700f3fa2b70f24aa8283db4
Rodolpho <rodolphocammarosano@gmail.com>
2022-08-22 23:25:58 +0200

    edit script server_static

--- serve_static.sh
+++ serve_static.sh
@@ -2,13 +2,13 @@ export VITE_HOST=127.0.0.1
 
 # display warning
 printf "\033[31;1m"
-echo "!! Host IP is set to" $VITE_HOST "- make sure this matches your .env file!!"
+echo "!! Host IP will be set to" $VITE_HOST "- make sure this matches your .env file!!"
 echo "If not, change the 1st line of serve_static.sh"
 printf "\033[39m\n"
 echo "This script does not install node modules (so do it first if you haven't done so yet)"
 
 # build frontend
-# cd frontend && npm run build && cd ..
+cd frontend && npm run build && cd .. # comment out this line to skip build
 
 # restart DB with a fresh one
 cd backend
@@ -18,10 +18,13 @@ docker-compose up -d
 # set env vars
 export FRONTEND_URL="" # overwriting this variable 
 export SERVE_STATIC=true
-# build backend
-# npm run build
+
+# build backend 
+npm run build # comment out to skip build
 
 # launch server
-# node dist/main.js
-npm run start:dev
+node dist/main.js
+
+# dev server: comment-out last 2 commands, uncomment the one below
+# npm run start:dev
 
