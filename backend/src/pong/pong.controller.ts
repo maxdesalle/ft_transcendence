6e51f994af9d04f88fa923a110e9a36366ba69d8
Rodolpho <rodolphocammarosano@gmail.com>
2022-08-23 02:54:42 +0200

    issue #11 player names on GET /pong/sessions

--- backend/src/pong/pong.controller.ts
+++ backend/src/pong/pong.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { UsersService } from 'src/users/users.service';
 import { sockets } from './pong.gateway';
+import { connected_users } from './pong.gateway';
 
 @Controller('pong')
 @ApiTags('pong')
+@UseGuards(JwtGuard)
 export class PongController {
+  constructor (private usersService: UsersService) {}
+
   @Get('sessions')
-  getSessions() {
-    return sockets.map((ob) => ob.id);
+  async getSessions() {
+    const res = [];
+    for (let s of sockets) {
+      const p1_id = connected_users.get(s.p1Socket);
+      const p2_id = connected_users.get(s.p2Socket);
+      const p1 = await this.usersService.findById(p1_id);
+      const p2 = await this.usersService.findById(p2_id);
+      res.push({
+        session_id: s.id,
+        p1: {
+          id: p1.id,
+          diplay_name: p1.display_name
+        },
+        p2: {
+          id: p2.id,
+          display_name: p2.display_name
+        }
+      });
+    }
+    return res;
   }
 }
